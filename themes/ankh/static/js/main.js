ba89c19f661e3b600babf8bc6f75d14ef4d12ad6
Jakob Jan Kamminga <jakobjankamminga@gmail.com>
2020-04-12 18:01:44 +0200

    Further improvements

--- themes/ankh/static/js/main.js
+++ themes/ankh/static/js/main.js
@@ -1,12 +1,15 @@
 function switchPortfolios() {
     var portfolioOne = document.getElementById('portfolio-1');
     var portfolioTwo = document.getElementById('portfolio-2');
-    if (portfolioOne.style.display === 'none') {
+    var checkBox = document.getElementById('portfolio-checkbox');
+    console.log("switchportfolios() ran" + checkBox);
+    if (checkBox.checked === true) {
         portfolioTwo.style.display = 'none';
         portfolioOne.style.display = 'flex';
     }
-    else if (portfolioTwo.style.display === 'none') {
+    else if (checkBox.checked === false) {
         portfolioOne.style.display = 'none';
         portfolioTwo.style.display = 'flex';
     }
 }
+document.addEventListener('DOMContentLoaded', switchPortfolios);
--- themes/ankh/static/js/main.ts
+++ themes/ankh/static/js/main.ts
@@ -1,12 +1,15 @@
 function switchPortfolios() {
     const portfolioOne: HTMLElement = document.getElementById('portfolio-1');
     const portfolioTwo: HTMLElement = document.getElementById('portfolio-2');
+    let checkBox = <HTMLInputElement> document.getElementById('portfolio-checkbox');
 
-    if (portfolioOne.style.display === 'none') {
+    if (checkBox.checked === true) {
         portfolioTwo.style.display = 'none';
         portfolioOne.style.display = 'flex';
-    } else if (portfolioTwo.style.display === 'none') {
+    } else if (checkBox.checked === false) {
         portfolioOne.style.display = 'none';
         portfolioTwo.style.display = 'flex';
     }
 }
+
+document.addEventListener('DOMContentLoaded', switchPortfolios);
