d0eb023f7e8219b6dcba2ee49c72bbcda6c8bb48
Jakob Jan Kamminga <jakobjankamminga@gmail.com>
2020-05-04 15:10:47 +0200

    Responsive improvements

--- themes/ankh/static/js/main.ts
+++ themes/ankh/static/js/main.ts
@@ -18,16 +18,35 @@ function toggleHamburger(hamburgerIcon: HTMLElement) {
     const headerNav: HTMLElement = document.getElementById('header-nav');
     const headerLeft: HTMLElement = document.getElementById('header-left');
     const headerRight: HTMLElement = document.getElementById('header-right');
+    const headerCenter: HTMLElement = document.getElementById('header-center');
     
-    if (hamburgerIcon.style.transform == 'rotate(90deg)') {
-        hamburgerIcon.style.transform = 'none';
+    if (hamburgerIcon.classList.contains('is-active')) {
+        hamburgerIcon.classList.remove('is-active');
+        hamburgerIcon.classList.add('is-inactive');
+        
         headerLeft.style.display = 'none';
         headerRight.style.display = 'none';
-        headerNav.style.display = 'none';
+        headerCenter.style.width = '33.33vw';
+        headerNav.style.display = 'flex';
+
+        headerNav.appendChild(headerLeft);
+        headerNav.appendChild(headerCenter);
+        headerNav.appendChild(headerRight);
+        
+
     } else {
-        hamburgerIcon.style.transform = 'rotate(90deg)';
+        hamburgerIcon.classList.remove('is-inactive');
+        hamburgerIcon.classList.add('is-active');
+        
+        headerNav.appendChild(headerRight);
+        headerNav.appendChild(headerLeft);
+
         headerLeft.style.display = 'block';
+        headerLeft.style.width = '100vw';
         headerRight.style.display = 'block';
-        headerNav.style.display = '80vh';
+        headerRight.style.width = '100vw';
+
+        headerNav.style.paddingBottom = '25px';
+        headerNav.style.flexWrap = 'wrap';
     }
 }
