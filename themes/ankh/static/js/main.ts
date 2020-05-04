e5fe214553f89a0288395ddd9c70523a69d022b8
Jakob Jan Kamminga <43784616+JJ-Kamminga@users.noreply.github.com>
2020-05-04 10:17:50 +0200

    Initial hamburger script idea

--- themes/ankh/static/js/main.ts
+++ themes/ankh/static/js/main.ts
@@ -14,5 +14,20 @@ function switchPortfolios() {
 
 document.addEventListener('DOMContentLoaded', switchPortfolios);
 
-function toggleHamburger() {
+function toggleHamburger(hamburgerIcon: HTMLElement) {
+    const headerNav: HTMLElement = document.getElementById('header-nav');
+    const headerLeft: HTMLElement = document.getElementById('header-left');
+    const headerRight: HTMLElement = document.getElementById('header-right');
+    
+    if (hamburgerIcon.style.transform == 'rotate(90deg)') {
+        hamburgerIcon.style.transform = 'none';
+        headerLeft.style.display = 'none';
+        headerRight.style.display = 'none';
+        headerNav.style.display = 'none';
+    } else {
+        hamburgerIcon.style.transform = 'rotate(90deg)';
+        headerLeft.style.display = 'block';
+        headerRight.style.display = 'block';
+        headerNav.style.display = '80vh';
+    }
 }
